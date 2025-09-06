5acad84be1a59c1078c0411dc53be563d509eb89
yaoxinyue-ee <yaoxinyue_ee@163.com>
2025-09-07 01:19:16 +0800

    苍穹外卖项目Day03完成“删除菜品”功能。并优化了 1.判断当前菜品是否能够删除 -- 是否存在起售中的菜品。根据count(*)返回启售数目判断是否删除成功  2.根据菜品id集合批量删除关联的口味数据和根据菜品id集合批量删除菜品数据

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,17 +1,18 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 菜品管理
@@ -38,4 +39,27 @@ public class DishController {
         return Result.success();
     }
 
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品分类查询:{}", dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     * @return
+     */
+    // 由Quuery参数传入的1,2,3是String类型,可以选择使用String接收再自己处理
+    // 但为了简便,直接使用Spring MVC解析处理接收到的参数ids,并且根据,分隔，分割出的每一个元素封装到List集合中
+    // 加入@RequestParam SpringMVC会动态解析字符串，并把id提取出来封装到集合中去
+    @DeleteMapping
+    @ApiOperation("菜品批量删除")
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("菜品批量删除:{}", ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +29,10 @@ public class DishServiceImpl implements DishService {
 
     @Autowired
     private DishMapper dishMapper;
-
     @Autowired
     private DishFlavorMapper dishFlavorMapper;
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
 
     /**
      * 新增菜品和对应口味数据
@@ -59,5 +69,66 @@ public class DishServiceImpl implements DishService {
             // Mybatis的动态sql支持批量插入<foreach>
             dishFlavorMapper.insertBatch(flavors);
         }
+    }
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        // 表示开始分页
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());
+        Page<DishVO> page = dishMapper.pageQuery(dishPageQueryDTO);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     */
+    @Transactional // 由于涉及多个数据表操作,因此需要添加事务注解
+    public void deleteBatch(List<Long> ids) {
+        // 判断当前菜品是否能够删除 -- 是否存在起售中的菜品
+//        for (Long id : ids) {
+//            Dish dish = dishMapper.getById(id);
+//            if (dish.getStatus() == StatusConstant.ENABLE){
+//                // 当前菜品处于起售中,不能删除
+//                throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+//            }
+//        }
+        // 这个ids传参是List<Long> ids，必须使用动态sql<foreach>遍历
+        // 因为#{ids}会将整个List作为单个参数处理，不会展开为多个值
+        int count = dishMapper.countByIdsAndStatus(ids);
+        if(count > 0){
+            throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+        }
+
+        // 判断当前菜品是否能够删除 -- 判断菜品是否被套餐关联
+        // 直接sql中子查询把状态和套餐都过滤掉,返回操作数据的条数,如果是0就说明不能删除,直接抛异常就行,如果>0再去删除dish_flavor表中的数据
+        // 这里可以直接返回count(0),然后在业务层直接判断是否等于0就行
+        List<Long> setmealIds = setmealDishMapper.getSetmealIdsByDishIds(ids);
+        if(setmealIds != null && setmealIds.size() > 0){
+            // 当前菜品被套餐关联了，不能删除
+            throw new DeletionNotAllowedException(MessageConstant.CATEGORY_BE_RELATED_BY_DISH);
+        }
+
+        // 先删除菜品关联的口味数据，再删菜品，防止口味删除失败导致外键为空
+
+        // 删除菜品表中的菜品数据
+        // 缺点:每一次for循环遍历都会发出两条sql,若遍历次数多,则删除数量多,可能引发性能方面问题
+//        for (Long id : ids) {
+//            dishMapper.deleteById(id);
+//            // 删除菜品关联的口味数据
+//            dishFlavorMapper.deleteByDishId(id);
+//        }
+
+
+        // 根据菜品id集合批量删除关联的口味数据
+        dishFlavorMapper.deleteByDishIds(ids);
+        // 根据菜品id集合批量删除菜品数据
+        dishMapper.deleteByIds(ids);
+
+
     }
 }
