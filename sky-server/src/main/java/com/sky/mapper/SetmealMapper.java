6072d6c10b94c5ae83d0c82527fcb221a872fcaf
yaoxinyue-ee <yaoxinyue_ee@163.com>
2025-09-09 22:19:34 +0800

    苍穹外卖项目Day04完成“完成套餐分页查询”功能和“删除套餐”功能。删除套餐中不仅要删除套餐表，还需要删除套餐和菜品关联表。

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,10 +1,16 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
@@ -29,4 +35,39 @@ public interface SetmealMapper {
      */
     @AutoFill(OperationType.INSERT)
     void insert(Setmeal setmeal);
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    /**
+     * 根据id查询套餐
+     * @param id
+     * @return
+     */
+    @Select("select * from setmeal where id = #{id}")
+    Setmeal getById(Long id);
+
+    /**
+     * 根据ids批量删除菜品
+     * @param id
+     */
+    @Delete("delete from setmeal where id = #{id}")
+    void deleteById(Long id);
+
+    /**
+     * 根据ids批量删除菜品
+     * @param ids
+     */
+    void deleteByIdBatch(List<Long> ids);
+
+    /**
+     * 根据ids查询启售数量
+     * @param ids
+     * @return
+     */
+    int countByIdsAndStatus(List<Long> ids);
 }
--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,14 +1,23 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -41,6 +50,55 @@ public class SetmealServiceImpl implements SetmealService {
             //保存套餐和菜品的关联关系,foreach批量插入
             setmealDishMapper.insertBatch(setmealDishes);
         }
+
+    }
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    /**
+     * 批量删除套餐
+     * 分两个循环：更安全，逻辑清楚 → 推荐
+     * 合并成一个循环：代码更简洁，但要完全依赖事务回滚，否则可能删到一半抛异常。
+     * @param ids
+     */
+    @Transactional // 处理两个表
+    public void deleteBatch(List<Long> ids) {
+        // 判断是否为启售状态，若不启售，则报错
+        // 根据id查询套餐启售状态
+//        ids.forEach(id -> {
+//            Setmeal setmeal = setmealMapper.getById(id);
+//            if(setmeal.getStatus() == StatusConstant.ENABLE){
+//                throw new DeletionNotAllowedException(MessageConstant.SETMEAL_ON_SALE);
+//            }
+//        });
+
+//        ids.forEach(setmealId -> {
+//            // 删除套餐表中的数据
+//            setmealMapper.deleteById(setmealId);
+//            // 删除套餐菜品关系表中的数据
+//            setmealDishMapper.deleteById(setmealId);
+//        });
+
+        // 判断ids中status=1的数量(count),大于0则说明有启售套餐,不能删除
+        int count = setmealMapper.countByIdsAndStatus(ids);
+        if(count > 0){
+            throw new DeletionNotAllowedException(MessageConstant.SETMEAL_ON_SALE);
+        }
+
+        // 批量删除动态sql foreach
+        setmealMapper.deleteByIdBatch(ids);
+        // 批量删除套餐和分类id绑定 setmeal_dish
+        setmealDishMapper.deleteBySetmealIdBatch(ids);
 
     }
 }
