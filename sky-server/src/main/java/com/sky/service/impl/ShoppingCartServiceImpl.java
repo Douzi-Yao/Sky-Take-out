7c26d26f8b3988a8507d98408efd0a05d1ba614b
yaoxinyue-ee <yaoxinyue_ee@163.com>
2025-09-22 17:11:59 +0800

    苍穹外卖项目Day07完成“添加购物车”功能。

---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,76 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    /**
+     * 添加购物车
+     * @param shoppingCartDTO
+     */
+    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        // 判断当前加入购物车的商品是否已经存在
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+
+        // 如果已经存在，将数量+1
+        if(list != null && list.size() > 0){
+            // 实际上list里应当只有一条数据，因为只会查到一条数据，对一条数据进行操作
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber() + 1); // 数量+1
+            // 执行update语句 update shopping_cart set number = ? where id = ?
+            // 这里实际上只需要更改数量，无需写成更新所有。因为shopping_cart表的其它字段实际上无法更改。
+            shoppingCartMapper.updateNumberById(cart);
+        }else{
+            // 如果不存在，需要插入一条购物车数据
+            // 此时需要构造一购物车对象，插入shopping_cart表
+            // 获取菜品名称、金额、图片，需要判断是dish表还是setmeal表
+            Long dishId = shoppingCartDTO.getDishId();
+            if(dishId != null){
+                // 本次添加到购物车的是菜品
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setAmount(dish.getPrice()); // 这里不用BeanUtils,因为属性名不对应
+                shoppingCart.setImage(dish.getImage());
+            }else{
+                // 本次添加到购物车的是套餐
+                Long setmealId = shoppingCartDTO.getSetmealId();
+                Setmeal setmeal = setmealMapper.getById(setmealId);
+
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setAmount(setmeal.getPrice());
+                shoppingCart.setImage(setmeal.getImage());
+            }
+            // 由于无论是套餐还是菜品均需要添加数量和创建时间，因此在条件判断外设置
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
+        }
+
+    }
+}
